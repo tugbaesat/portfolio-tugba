902303326cb7ce5d0cf9cbc24ed865f6f60f11f9
Tugba Esat <tugbaesat007@gmail.com>
2023-10-16 14:00:54 -0700

    fix footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='pt-6 pb-2 text-center bg-gray-100'>
-            <span className='text-xs text-[#9ca2a9] italic '>Coded by Tugba Esat Sahin</span>
+            <span className='text-xs text-[#9ca2a9] italic '>Tugba Esat Sahin ©</span>
     </footer>
   )
 }
