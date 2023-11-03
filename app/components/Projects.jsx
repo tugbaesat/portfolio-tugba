59ae2676d885002592569128cc15874bf12fbc8b
Tugba Esat <tugbaesat007@gmail.com>
2023-11-03 14:02:34 -0700

    new project added

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -1,14 +1,15 @@
-import projectImg1 from "../../public/images/projects/project1.png";
-import projectImg2 from "../../public/images/projects/project2.png";
-import projectImg3 from "../../public/images/projects/project3.png";
-import projectImg4 from "../../public/images/projects/project4.png";
-import projectImg5 from "../../public/images/projects/project5.png";
-import projectImg6 from "../../public/images/projects/project6.png";
-import projectImg7 from "../../public/images/projects/project7.png";
-import projectImg8 from "../../public/images/projects/project8.png";
-import projectImg9 from "../../public/images/projects/project9.png";
-import projectImg10 from "../../public/images/projects/project10.png";
-import projectImg11 from "../../public/images/projects/project11.png";
+import learningApp from "../../public/images/projects/learning-app.png";
+import movieApp from "../../public/images/projects/movie-project.png";
+import movieNomination from "../../public/images/projects/movie-nomination.png";
+import tipCalculator from "../../public/images/projects/tip-calculator.png";
+import simonGame from "../../public/images/projects/simon-game.png";
+import diceeApp from "../../public/images/projects/dicee-app.png";
+import drumKit from "../../public/images/projects/drum-kit.png";
+import clipboard from "../../public/images/projects/clipboard-landing.png";
+import blogr from "../../public/images/projects/blogr-landing.png";
+import reactForm from "../../public/images/projects/react-form-app.png";
+import tasks from "../../public/images/projects/tasks-app.png";
+import shopping from "../../public/images/projects/shopping-app.png";
 
 const projectData = [
   {
@@ -16,7 +17,7 @@ const projectData = [
     title: "Learning App",
     description:
       "Learning App web application is built using React with Next.js, Tailwind CSS for styling, and MongoDB for data storage.",
-    image: projectImg1,
+    image: learningApp,
     gitUrl: "https://github.com/tugbaesat/Learnification",
     previewUrl: "https://learn-u-team-7.vercel.app/",
   },
@@ -24,90 +25,99 @@ const projectData = [
     id: 2,
     title: "Movie App",
     description:
-      "Movie App is a movie database project and built by using The Movie DB API.",
-    image: projectImg2,
+      "Movie App is a movie database project and built by using the Movie DB API.",
+    image: movieApp,
     gitUrl: "https://github.com/tugbaesat/movie-project",
     previewUrl: "https://movie-project-tugbaesat.vercel.app/",
   },
   {
     id: 3,
+    title: "Movie Nomination App",
+    description:
+      "Movie Nomination App is built using React with Next.js by using the OMDB API.",
+    image: movieNomination,
+    gitUrl: "https://github.com/tugbaesat/movie-nomination",
+    previewUrl: "https://movie-nomination.vercel.app",
+  },
+  {
+    id: 4,
     title: "Tip Calculator App",
     description:
       "Tip Calculator App is built using React with Next.js.",
-    image: projectImg3,
+    image: tipCalculator,
     gitUrl: "https://github.com/tugbaesat/nextjs-tip-calculator",
     previewUrl: "https://nextjs-tip-calculator.vercel.app/",
   },
   {
-    id: 4,
+    id: 5,
     title: "Simon Game",
     description:
       "This game app is built using Javascript.",
-    image: projectImg4,
+    image: simonGame,
     gitUrl: "https://github.com/tugbaesat/simon-game",
     previewUrl: "https://simon-game-tugbaesat.vercel.app/",
   },
   {
-    id: 5,
+    id: 6,
     title: "Dicee App",
     description:
       "This app is built using Javascript.",
-    image: projectImg5,
+    image: diceeApp,
     gitUrl: "https://github.com/tugbaesat/dicee-challenge",
     previewUrl: "https://dicee-challenge-tugbaesat.vercel.app/",
   },
   {
-    id: 6,
+    id: 7,
     title: "Drum Kit",
     description:
       "This app is built using Javascript.",
-    image: projectImg6,
+    image: drumKit,
     gitUrl: "https://github.com/tugbaesat/drum-kit",
     previewUrl: "https://drum-kit-tugbaesat.vercel.app",
   },
   {
-    id: 7,
+    id: 8,
     title: "Clipboard Landing Page",
     description:
       "This is a Frontend Mentor landing page clone project.",
-    image: projectImg7,
+    image: clipboard,
     gitUrl: "https://github.com/tugbaesat/clipboard-landing-page-challenge",
     previewUrl: "https://clipboard-landing-page-challenge.vercel.app",
   },
   {
-    id: 8,
+    id: 9,
     title: "Blogr Landing Page",
     description:
       "This is a Frontend Mentor landing page clone project.",
-    image: projectImg8,
+    image: blogr,
     gitUrl: "https://github.com/tugbaesat/landing-page-challenge",
     previewUrl: "https://blogr-landing-page-tugbaesat.vercel.app",
   },
   {
-    id: 9,
+    id: 10,
     title: "React Form App",
     description:
       "React Form App is built using React with Next.js. This is a React Hook Form project.",
-    image: projectImg9,
+    image: reactForm,
     gitUrl: "https://github.com/tugbaesat/react-form-app",
     previewUrl: "https://react-form-tugbaesat.vercel.app",
   },
   {
-    id: 10,
+    id: 11,
     title: "Tasks App",
     description:
       "Tasks App is built using React.",
-    image: projectImg10,
+    image: tasks,
     gitUrl:
       "https://github.com/tugbaesat/react-hooks-state-events-mini-project",
     previewUrl: "https://tasks-app-tugbaesat.vercel.app",
   },
   {
-    id: 11,
+    id: 12,
     title: "Shopping List App",
     description:
       "Shopping List App is built using React.",
-    image: projectImg11,
+    image: shopping,
     gitUrl:
       "https://github.com/tugbaesat/react-hooks-state-and-events-lab/blob/master/README.md",
     previewUrl: "https://react-hooks-state-and-events-lab.vercel.app",
